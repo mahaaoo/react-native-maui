e841a24fb1b5f2548ec3d53b3e234804fef3b33d
mahao <haoma@newbanker.cn>
2022-06-11 11:11:56 +0800

    fix opacity animation

--- src/components/ImageViewer/ImageOverlay.tsx
+++ src/components/ImageViewer/ImageOverlay.tsx
@@ -1,10 +1,11 @@
 import React, { forwardRef, useCallback, useEffect, useImperativeHandle } from 'react';
-import { Dimensions, Image, TouchableOpacity, View, StyleSheet } from 'react-native';
+import { Dimensions, TouchableOpacity, View, StyleSheet } from 'react-native';
 import { Gesture, GestureDetector } from 'react-native-gesture-handler';
-import Animated, { interpolate, runOnJS, useAnimatedReaction, useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated';
+import Animated, { interpolate, runOnJS, useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated';
 import { snapPoint } from 'react-native-redash';
 import { useOverlay } from '../Overlay';
 import { Position } from './ImageContainer';
+import Display from './Display';
 
 const {width: Width, height: Height} = Dimensions.get('window');
 
@@ -93,8 +94,9 @@ const ImageOverlay = forwardRef<ImageOverlayRef, ImageOverlayProps>((props, ref)
     offsetY.value = translateY.value;
   })
   .onUpdate(({translationY}) => {
-    translateY.value = translationY + offsetY.value;    
-    opacity.value = withTiming(Math.abs(translateY.value) * 5 / Height, {duration});
+    translateY.value = translationY + offsetY.value;   
+     
+    opacity.value = withTiming(interpolate(translateY.value, [-0.3*Height, 0, 0.3 * Height], [0.5, 1, 0.5]), {duration});
   })
   .onEnd(({velocityY}) => {
     const destY = snapPoint(translateY.value, velocityY, snapPointsY);
@@ -102,10 +104,9 @@ const ImageOverlay = forwardRef<ImageOverlayRef, ImageOverlayProps>((props, ref)
     if (Math.abs(destY) >= 0.25 * Height) {
       willUnMount.value = true;
       runOnJS(remove)(innerKey);  
-    } else {
-      translateY.value = withTiming(0, {duration});
-      opacity.value = withTiming(1, {duration});
     }
+    translateY.value = withTiming(0, {duration});
+    opacity.value = withTiming(1, {duration});
   })
 
   const singleTap = Gesture.Tap()
@@ -154,7 +155,7 @@ const ImageOverlay = forwardRef<ImageOverlayRef, ImageOverlayProps>((props, ref)
               data.map((item, index) => {
                 const position = positionList[index];
                 return (
-                        <Content 
+                        <Display 
                           key={`Content_${index}`}
                           containerTranslateY={translateY}
                           currentIndex={scrollIndex} 
@@ -170,82 +171,6 @@ const ImageOverlay = forwardRef<ImageOverlayRef, ImageOverlayProps>((props, ref)
   )
 });
 
-interface ContentProps {
-  position: Position,
-  duration?: number,
-  paddingTop: number;
-  paddingBottom: number;
-  item: any;
-  currentIndex: Animated.SharedValue<number>;
-  index: number;
-  willUnMount: Animated.SharedValue<boolean>;
-  containerTranslateY: Animated.SharedValue<number>;
-}
-
-const Content: React.FC<ContentProps> = (props) => {
-  const {position, duration, paddingTop, paddingBottom, item, currentIndex, index, willUnMount, containerTranslateY} = props;
-  const { width: w, height: h, pageX: x, pageY: y } = position;
-  
-  const width = useSharedValue(w);
-  const height = useSharedValue(h);
-  const translateX = useSharedValue(x)
-  const translateY = useSharedValue(y - paddingTop);
-  const scale = useSharedValue(1);
-
-  useAnimatedReaction(() => willUnMount.value, (value) => {
-    if (value && index === currentIndex.value) {
-      console.log('即将关闭的', position);
-
-      width.value = withTiming(w, {duration});
-      height.value = withTiming(h, {duration});
-      translateX.value = withTiming(x, {duration});
-      translateY.value = withTiming(y - paddingTop, {duration});  
-    }
-  });
-
-  useAnimatedReaction(() => containerTranslateY.value, (value) => {
-    const ratio = interpolate(value, [-Height/2, 0, Height / 2], [0.5, 1, 0.5]);
-    scale.value = withTiming(ratio, {duration});
-  })
-
-  const animationStyle = useAnimatedStyle(() => {
-    return {
-      width: width.value,
-      height: height.value,
-      transform: [{
-        translateX: translateX.value
-      }, {
-        translateY: translateY.value + containerTranslateY.value
-      }, {
-        scale: scale.value
-      }]
-    }
-  })
-
-  useEffect(() => {
-    console.log('currentIndex.value', currentIndex.value)
-    if (currentIndex.value === index) {
-      width.value = withTiming(Width, {duration});
-      height.value = withTiming(Height - paddingTop - paddingBottom, {duration});
-      translateX.value = withTiming(0, {duration});
-      translateY.value = withTiming(0, {duration});  
-    } else {
-      width.value = Width;
-      height.value = Height - paddingTop - paddingBottom;
-      translateX.value = 0;
-      translateY.value = 0;  
-    }
-  }, [])
-
-  return (
-    <View style={{ flex: 1, width: Width }}>
-      <Animated.View style={[animationStyle]}>
-        <Image source={item.source} style={{ width: '100%', height: '100%' }} resizeMode="contain" />
-      </Animated.View>  
-    </View>
-  )
-}
-
 const styles = StyleSheet.create({
   overlay: {
     ...StyleSheet.absoluteFillObject,
@@ -253,5 +178,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1, 
   }
-})
+});
+
 export default ImageOverlay;
