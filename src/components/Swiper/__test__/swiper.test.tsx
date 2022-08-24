3a290dca2d325c4fb0facd8a5d54304f63a4f6f7
mahao <mahao9393@163.com>
2022-08-24 13:59:38 +0800

    update test cases

--- src/components/Swiper/__test__/swiper.test.tsx
+++ src/components/Swiper/__test__/swiper.test.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { render } from '@testing-library/react-native';
+import { act, render } from '@testing-library/react-native';
 import { withReanimatedTimer } from 'react-native-reanimated/src/reanimated2/jestUtils';
 import { Dimensions, Image } from 'react-native';
 import { Swiper } from '../index';
@@ -24,8 +24,11 @@ describe('Testing:Swiper', () => {
         },
       ];
 
+      let currentIndex = 0;
       const onScollStartMock = jest.fn();
-      const onScollEndMock = jest.fn();
+      const onScollEndMock = (index: number) => {
+        currentIndex = index;
+      };
 
       const previousMock = jest.fn();
       const nextMock = jest.fn();
@@ -38,39 +41,51 @@ describe('Testing:Swiper', () => {
         },
       };
 
-      render(
-        <Swiper
-          ref={useRef}
-          interval={1000}
-          dataSource={card}
-          renderItem={(item) => {
-            return (
-              <Image
-                source={item.source}
-                style={{ width: '100%', height: '100%' }}
-              />
-            );
-          }}
-          onScollStart={onScollStartMock}
-          onScollEnd={onScollEndMock}
-          auto={false}
-          horizontal={true}
-          style={{
-            width,
-            height: 200,
-          }}
-        />
-      );
+      function SwiperComponent() {
+        return (
+          <Swiper
+            ref={useRef}
+            interval={1000}
+            dataSource={card}
+            renderItem={(item) => {
+              return (
+                <Image
+                  source={item.source}
+                  style={{ width: '100%', height: '100%' }}
+                />
+              );
+            }}
+            onScollStart={onScollStartMock}
+            onScollEnd={onScollEndMock}
+            auto={false}
+            horizontal={true}
+            style={{
+              width,
+              height: 200,
+            }}
+          />
+        );
+      }
 
-      // const swiper = getByTestId('test-swiper');
-      // console.log(swiper);
+      const swiperRender = render(<SwiperComponent />);
 
-      // expect(getCurrentIndexMock).toHaveBeenCalled();
+      expect(currentIndex).toBe(0);
 
-      useRef.current.previous();
+      act(() => {
+        useRef.current.next();
+      });
 
-      // expect(onScollStartMock).toHaveBeenCalled();
-      // expect(onScollEndMock).toHaveBeenCalled();
+      expect(currentIndex).toBe(0);
+
+      // // const swiper = getByTestId('test-swiper');
+      // // console.log(swiper);
+
+      // // expect(getCurrentIndexMock).toHaveBeenCalled();
+
+      // useRef.current.previous();
+
+      // // expect(onScollStartMock).toHaveBeenCalled();
+      // // expect(onScollEndMock).toHaveBeenCalled();
     });
   });
 });
