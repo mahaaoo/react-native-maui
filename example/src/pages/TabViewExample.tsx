2eb59e1327a05b4cab2dd5129a64962529822cd7
马浩 <303527871@qq.com>
2022-08-26 00:13:53 +0800

    tabbar container offset

--- example/src/pages/TabViewExample.tsx
+++ example/src/pages/TabViewExample.tsx
@@ -1,25 +1,40 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import { View, StyleSheet, Text, Dimensions } from 'react-native';
 import { Gesture, GestureDetector } from 'react-native-gesture-handler';
 import Animated, {
+  Extrapolate,
   useAnimatedStyle,
   withTiming,
+  useSharedValue,
+  interpolate,
 } from 'react-native-reanimated';
-import { useSharedValue } from 'react-native-reanimated';
-import { snapPoint } from 'react-native-maui';
 
 const { width } = Dimensions.get('window');
 
 interface TabViewExampleProps {}
 
-const TabList = ['Tab1', 'Tab2', 'Tab3', 'Tab4', 'Tab5'];
-const TabListColor = ['orange', '#666', 'cyan', '#e82c1c', 'purple'];
+const TabList = ['Tab1', 'Tab2', 'Tab3', 'Tab4', 'Tab5', 'Tab6', 'Tab7'];
+const TabListColor = [
+  'orange',
+  '#666',
+  'cyan',
+  '#e82c1c',
+  'purple',
+  'orange',
+  '#666',
+];
+const TOTAL_WIDTH = width * TabList.length;
+const TABBAR_WIDTH = 100;
 
 const TabViewExample: React.FC<TabViewExampleProps> = (props) => {
   const {} = props;
+
   const translateX = useSharedValue(0);
   const offsetX = useSharedValue(0);
-  const snapPoints = TabList.map((_, index) => -index * width);
+  const currentIndex = useSharedValue(0);
+  const containerOffset = useSharedValue(0);
+  const tabOffset = useSharedValue(0);
 
   const panGesture = Gesture.Pan()
     .onBegin(() => {
@@ -29,8 +44,45 @@ const TabViewExample: React.FC<TabViewExampleProps> = (props) => {
       translateX.value = translationX + offsetX.value;
     })
     .onEnd(({ velocityX }) => {
-      const dest = snapPoint(translateX.value, velocityX, snapPoints);
-      translateX.value = withTiming(dest);
+      const page = -(translateX.value + 0.2 * velocityX) / width;
+      let nextIndex = interpolate(
+        page,
+        [
+          currentIndex.value - 0.5,
+          currentIndex.value,
+          currentIndex.value + 0.5,
+        ],
+        [currentIndex.value - 1, currentIndex.value, currentIndex.value + 1],
+        Extrapolate.CLAMP
+      );
+
+      if (nextIndex < 0) nextIndex = 0;
+      if (nextIndex > TabList.length - 1) nextIndex = TabList.length - 1;
+
+      nextIndex = Math.round(nextIndex);
+
+      let dest = interpolate(
+        -Math.round(nextIndex) * width,
+        [0, -TOTAL_WIDTH],
+        [0, TABBAR_WIDTH * TabList.length]
+      );
+
+      if (dest > width / 2) {
+        const adjust = width - TABBAR_WIDTH * (nextIndex + 1 + 1);
+        dest += adjust;
+        if (nextIndex + 1 + 1 <= TabList.length) {
+          containerOffset.value = withTiming(adjust);
+        } else {
+          dest += TABBAR_WIDTH;
+        }
+      } else {
+        containerOffset.value = withTiming(0);
+      }
+
+      tabOffset.value = withTiming(dest);
+      translateX.value = withTiming(-nextIndex * width, {}, () => {
+        currentIndex.value = nextIndex;
+      });
     });
 
   const animatedStyle = useAnimatedStyle(() => {
@@ -43,31 +95,72 @@ const TabViewExample: React.FC<TabViewExampleProps> = (props) => {
     };
   });
 
+  const tabContainerStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateX: containerOffset.value,
+        },
+      ],
+    };
+  });
+
+  const tarbarStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateX: tabOffset.value,
+        },
+      ],
+    };
+  });
+
   return (
     <View style={styles.container}>
-      <View
-        style={{
-          flexDirection: 'row',
-          height: 50,
-          backgroundColor: 'white',
-          alignItems: 'center',
-        }}
+      <Animated.View
+        style={[
+          {
+            flexDirection: 'row',
+            backgroundColor: 'white',
+            alignItems: 'center',
+            width: TABBAR_WIDTH * TabList.length,
+          },
+          tabContainerStyle,
+        ]}
       >
         {TabList.map((item, index) => {
           return (
-            <View style={{ marginHorizontal: 30 }}>
+            <View
+              style={{
+                width: TABBAR_WIDTH,
+                height: 50,
+                alignItems: 'center',
+                justifyContent: 'center',
+                borderWidth: 1,
+              }}
+            >
               <Text>{item}</Text>
             </View>
           );
         })}
-      </View>
+      </Animated.View>
+      <Animated.View
+        style={[
+          {
+            height: 4,
+            width: TABBAR_WIDTH,
+            backgroundColor: 'blue',
+          },
+          tarbarStyle,
+        ]}
+      />
       <GestureDetector gesture={panGesture}>
         <Animated.View
           style={[
             {
               flex: 1,
               flexDirection: 'row',
-              width: width * TabList.length,
+              width: TOTAL_WIDTH,
             },
             animatedStyle,
           ]}
@@ -95,6 +188,7 @@ const TabViewExample: React.FC<TabViewExampleProps> = (props) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    overflow: 'hidden',
   },
 });
 
