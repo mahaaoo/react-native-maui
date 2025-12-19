780707777518fbc4877045e301ad1efa281a51e1
ma <mahao9393@163.com>
2025-12-19 17:24:11 +0800

    fix: react依赖冲突

--- src/components/Carousel/__test__/hook.test.ts
+++ src/components/Carousel/__test__/hook.test.ts
@@ -1,4 +1,4 @@
-import { renderHook } from '@testing-library/react-hooks';
+import { renderHook } from '@testing-library/react-native';
 import { withReanimatedTimer } from 'react-native-reanimated/src/reanimated2/jestUtils';
 import {
   useProps,
--- src/components/Picker/__test__/hook.test.ts
+++ src/components/Picker/__test__/hook.test.ts
@@ -1,4 +1,4 @@
-import { renderHook } from '@testing-library/react-hooks';
+import { renderHook } from '@testing-library/react-native';
 import { Easing } from 'react-native-reanimated';
 import { useProps, useInitialValue } from '../utils';
 
